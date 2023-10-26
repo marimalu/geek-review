256e2cda36d8af77af0f0e0bb2560cf204f2f9d2
mosiah-adam <mosiah.adam@escolar.ifrn.edu.br>
2023-10-26 15:57:24 -0300

    Pequenas mudanças nas cores e nomes

--- components/components.js
+++ components/components.js
@@ -1,10 +1,12 @@
 import Footer from "./Footer.js"
 import Header from "./Header.js"
 import MiddleCard from "./MiddleCard.js"
+import PopularReview from './PopularReview.js'
 
 if ('customElements' in window)
 {
     customElements.define('main-footer', Footer)
     customElements.define('main-header', Header)
     customElements.define('middle-card', MiddleCard)
+    customElements.define('popular-review', PopularReview)
 }
